17a2373b7797eb87a0db5fb55222d13b940de2d0
jonny_ji7 <jonny@wwad.de>
2022-05-29 11:33:41 +0200

    Control outputs with open/close buttons
    - Uncomment call of control and gate handle functions
    - Add function to select whether left or right gate is controlled by
      open/close buttons by pressing both buttons at the same time (beeps
      1s)
    - Add function that controls two gpio pins with open/close
      buttons (beeps while any output is on)

--- main/main.cpp
+++ main/main.cpp
@@ -28,12 +28,10 @@ uint32_t timestamp_debugOutput = esp_log_timestamp();
 void statusReport(){
     
     ESP_LOGI(TAG, "============= STATUS REPORT =============");
-    ESP_LOGI(TAG, "BUTTONS: S8 button_open=%d  |  S7 button_close=%d", buttonOpen.state, buttonClose.state);
-    ESP_LOGI(TAG, "CONTROL: State=%s  |  count=%d  |  lastAction=%d", controlStateStr[(int)ctlState], countPressed, timestampLastAction);
-    ESP_LOGI(TAG, "RIGHT GATE: State=%s  |  S1 open=%d  |  S2 closed=%d  |  K1 opening=  |  K2 closing=",
-            gateStateStr[(int)gateRight.state], gpio_get_level(GPIO_B_RIGHT_OPEN), gpio_get_level(GPIO_B_RIGHT_CLOSED));
-    ESP_LOGI(TAG, "LEFT GATE:  State=%s  |  S3 open=%d  |  S4 closed=%d  |  K1 opening=?  |  K2 closing=?",
-            gateStateStr[(int)gateRight.state], gpio_get_level(GPIO_B_LEFT_OPEN), gpio_get_level(GPIO_B_LEFT_CLOSED));
+    ESP_LOGI(TAG, "BUTTONS (evaluated): S8 button_open=%d  |  S7 button_close=%d", buttonOpen.state, buttonClose.state);
+    ESP_LOGI(TAG, "BUTTONS (gpioLevel): S8 button_open=%d  |  S7 button_close=%d", gpio_get_level(GPIO_S_OPEN), gpio_get_level(GPIO_S_CLOSE));
+    ESP_LOGI(TAG, "RIGHT GATE:  S1 open=%d  |  S2 closed=%d  |  K1 opening=?  |  K2 closing=?", gpio_get_level(GPIO_B_RIGHT_OPEN), gpio_get_level(GPIO_B_RIGHT_CLOSED));
+    ESP_LOGI(TAG, "LEFT GATE:   S3 open=%d  |  S4 closed=%d  |  K1 opening=?  |  K2 closing=?", gpio_get_level(GPIO_B_LEFT_OPEN), gpio_get_level(GPIO_B_LEFT_CLOSED));
     ESP_LOGI(TAG, "=========================================");
     timestamp_debugOutput = esp_log_timestamp();
 }
@@ -41,14 +39,47 @@ void statusReport(){
 
 
 
+//function to control gate/two outputs via open/close button
+void controlButtonGate(gpio_num_t gpio_relayOpen, gpio_num_t gpio_relayClose){
+    //open
+    if (gpio_get_level(GPIO_S_OPEN) == 1){
+        gpio_set_level(gpio_relayOpen, 1);
+        //beep when pin is on
+        gpio_set_level(GPIO_NUM_12, 1);
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_NUM_12, 0);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }else{
+        gpio_set_level(gpio_relayOpen, 0);
+    }
+    //close
+    if (gpio_get_level(GPIO_S_CLOSE) == 1){
+        gpio_set_level(gpio_relayClose, 1);
+        //beep when pin is on
+        gpio_set_level(GPIO_NUM_12, 1);
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_NUM_12, 0);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }else{
+        gpio_set_level(gpio_relayClose, 0);
+    }
+}
 
 
 extern "C" void app_main(void)
 {
 
+    //=================================
+    //===== Variable declarations =====
+    //=================================
+    //variables for switching between the two gates
+    bool gateSelect = false;
+    bool switched = false;
+
+
 
     while (1){
-        vTaskDelay(10 / portTICK_PERIOD_MS); //slight delay is required otherwise watchdog will be triggered
+        vTaskDelay(100 / portTICK_PERIOD_MS); //slight delay is required otherwise watchdog will be triggered
         
         //run handle function for buttons TODO: run these in another task?
         //note: the switch objects are declared and configured in config.hpp
@@ -56,7 +87,7 @@ extern "C" void app_main(void)
         buttonClose.handle();
 
 
-        //testing evaluated switch - log button events
+        //====== log button press/release events from evaluated switch object ======
         if (buttonOpen.risingEdge){
             ESP_LOGI(TAG, "== Button open pressed == - time Released: %d", buttonOpen.msReleased);
         }else if (buttonOpen.fallingEdge){
@@ -70,17 +101,48 @@ extern "C" void app_main(void)
         }
 
 
-        //run function that handles buttons and controls the gate
-        control();
 
-        //run handle function for gates
-        gateRight.handle();
-        gateLeft.handle();
+
+        //====== switch to controlling other gate when pressing both buttons ======
+        if (gpio_get_level(GPIO_S_OPEN) == 1 && gpio_get_level(GPIO_S_CLOSE) == 1 && switched == false){
+            //lock
+            switched = true;
+            //invert selected gate
+            gateSelect = !gateSelect;
+            //beep buzzer
+            gpio_set_level(GPIO_NUM_12, 1);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            gpio_set_level(GPIO_NUM_12, 0);
+        }else{
+            //release lock
+            switched = false;
+        }
+
+
+
+        //======= control selected gate via open/close buttons ========
+        if (gateSelect){
+            //control left gate via buttons
+            controlButtonGate(GPIO_K_OPEN_LEFT, GPIO_K_CLOSE_LEFT);
+        }else{
+            //control right gate via buttons
+            controlButtonGate(GPIO_K_OPEN_RIGHT, GPIO_K_CLOSE_RIGHT);
+        }
+
+
 
         //show debug output in certain time intervals
-        if (esp_log_timestamp() - timestamp_debugOutput > 5000){
+        if (esp_log_timestamp() - timestamp_debugOutput > 3000){
             statusReport();
         }
+
+//        //run function that handles buttons and controls the gate
+//        control();
+//
+//        //run handle function for gates
+//        gateRight.handle();
+//        gateLeft.handle();
+//
 
     }
 
